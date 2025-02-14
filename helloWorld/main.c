86e93ef357d71455220770fef475623d37e1317e
Artur0campos <arturcamposcamara@gmail.com>
2025-02-13 21:34:12 -0300

    Initial Commit

---
+++ helloWorld/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  helloWorld
+//
+//  Created by Artur Campos on 13/02/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!");
+    return 0;
+}
